ab3addca38733c02c4c7848f4657ab17cef3435b
forana <alexdforan@gmail.com>
2013-11-26 01:37:42 -0600

    SiR

--- app.js
+++ app.js
@@ -19,11 +19,17 @@ app.get(/^\/([0-9]+)x([0-9]+)(x[0-9]+)?(:[0-9]+)?/, function(req, res) {
 	var sites = req.params[2] ? req.params[2].substring(1) : 25;
 	var border = req.params[3] ? req.params[3].substring(1) : 2;
 
-	var xml = builder.generateVoronoiSVG(width, height, sites, border);
+	if (sites > 500 || width > 200000 || height > 200000) {
+		res.send("too big, cannot deal", 500);
+	} else if (sites < 2 || width < 1 || height < 1) {
+		res.send("something's too small", 500);
+	} else {
+		var xml = builder.generateVoronoiSVG(width, height, sites, border);
 
-	res.setHeader("Content-Type", "image/svg+xml");
-	res.setHeader("Content-Length", xml.length);
-	res.end(xml);
+		res.setHeader("Content-Type", "image/svg+xml");
+		res.setHeader("Content-Length", xml.length);
+		res.end(xml);
+	}
 });
 
 app.listen(port);
